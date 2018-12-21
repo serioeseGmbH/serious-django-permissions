cebaee50f72f27e77d9e4a549de060bf5337fbbc
Simon Welker <simon@serioese.gmbh>
2018-12-21 18:13:47 +0100

    chore(README): add pip install instruction

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='serious-django-permissions',
-    version='0.2',
+    version='0.2.post1',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
