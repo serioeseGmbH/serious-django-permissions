fefbe0e50154a45067433cbcfae2488090c1d8c8
Sebastian Latacz <sebastian@gitmate.io>
2019-01-24 16:59:01 +0100

    Add GlobalLevelTests

--- test_project/test_app/tests.py
+++ test_project/test_app/tests.py
@@ -6,14 +6,13 @@ from django.contrib.auth.models import Permission
 from serious_django_permissions.management.commands import create_permissions, create_groups
 
 from .models import RestrictedModel, UnrestrictedModel
-from .permissions import RestrictedModelPermission
+from .permissions import RestrictedModelPermission, GlobalPermission
 from .groups import AuthorizedGroup
 from .views import restricted_view
 
 class ManageFunctionTests(TestCase):
 
     def test_create_user_permissions(self):
-
         perm = Permission.objects.filter(codename=RestrictedModelPermission.codename)
         self.assertFalse(perm, 'The permission should not exist yet, but it does.')
 
@@ -30,6 +29,15 @@ class ManageFunctionTests(TestCase):
 
         perm = Group.objects.filter(name=AuthorizedGroup.group_name)
         self.assertTrue(perm, 'The group should not exist yet, but it does.')
+
+    def test_create_global_permission(self):
+        perm = Permission.objects.filter(codename=GlobalPermission.codename)
+        self.assertFalse(perm, 'The permission should not exist yet, but it does.')
+
+        create_permissions.Command().handle()
+
+        perm = Permission.objects.filter(codename=GlobalPermission.codename)
+        self.assertTrue(perm, 'The permission should exist, but it doesnt.')
 
 
 class UserLevelTests(TestCase):
@@ -89,6 +97,42 @@ class GroupLevelTests(TestCase):
 
     def test_unauthorized_user_does_not_have_permission(self):
         self.assertFalse(self.unauthorized_user.has_perm(RestrictedModelPermission))
+
+    def test_unauthorized_user_accessing_view(self):
+        request = self.factory.get('/restricted_view')
+        request.user = self.unauthorized_user
+        response = restricted_view(request)
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_authorized_user_accessing_view(self):
+        request = self.factory.get('/restricted_view')
+        request.user = self.authorized_user
+        response = restricted_view(request)
+
+        self.assertEqual(response.status_code, 200)
+
+class GlobalLevelTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.authorized_user = get_user_model().objects.create(
+            username='authorized_user'
+        )
+        perm, created_at = GlobalPermission.get_or_create()
+        cls.authorized_user.user_permissions.add(perm)
+
+        cls.unauthorized_user = get_user_model().objects.create(
+            username='unauthorized_user'
+        )
+
+        cls.factory = RequestFactory()
+
+    def test_unauthorized_user_does_not_have_permission(self):
+        self.assertFalse(self.unauthorized_user.has_perm(GlobalPermission))
+
+    def test_authorized_user_has_permission(self):
+        self.assertTrue(self.authorized_user.has_perm(GlobalPermission))
 
     def test_unauthorized_user_accessing_view(self):
         request = self.factory.get('/restricted_view')
