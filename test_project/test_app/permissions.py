bd65fe2020d6535a08eebfdb29953eb8dc4b42e6
Simon Welker <simon@serioese.gmbh>
2019-01-27 19:11:17 +0100

    refactor(tests): explicitly refer to Permission class in restricted view, this is now possible

--- test_project/test_app/permissions.py
+++ test_project/test_app/permissions.py
@@ -1,11 +1,10 @@
 from serious_django_permissions.permissions import Permission
 
-from .models import RestrictedModel
-
 
 class RestrictedModelPermission(Permission):
     model = 'RestrictedModel'
     description = 'Enables the user to write in RestrictedModel'
+
 
 class GlobalPermission(Permission):
     model = None
--- test_project/test_app/views.py
+++ test_project/test_app/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
 
+from .permissions import RestrictedModelPermission, GlobalPermission
 
-@permission_required('test_app.restricted_model')
+@permission_required(RestrictedModelPermission)
 def restricted_view(request):
     return HttpResponse("You've accessed a restricted_view")
