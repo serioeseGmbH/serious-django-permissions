5706ff2ac1c5130cade01bea5584c314c7633b38
Sebastian Latacz <sebastian@gitmate.io>
2019-02-06 13:54:54 +0100

    wip(Commands): Move creation logic to helpers and add create_groups()
    This is wip, requesting feedback on the approach.
    The tests fail due to a circular import. This will be fixed once
    the approach is settled.
    
    Regards #5

--- serious_django_permissions/helpers.py
+++ serious_django_permissions/helpers.py
@@ -1,4 +1,13 @@
 import re
+from abc import ABC
+import importlib
+import inspect
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from .permissions import Permission
+from .groups import Group
 
 # https://stackoverflow.com/a/1176023/3090225
 first_cap_re = re.compile('(.)([A-Z][a-z]+)')
@@ -6,3 +15,37 @@ all_cap_re = re.compile('([a-z0-9])([A-Z])')
 def camel_to_snake(name):
     s1 = first_cap_re.sub(r'\1_\2', name)
     return all_cap_re.sub(r'\1_\2', s1).lower()
+
+
+def create_permissions(self, *args, **options):
+    for app in settings.INSTALLED_APPS:
+            lib = importlib.util.find_spec("{}.permissions".format(app))
+            if lib:
+                lib = importlib.import_module("{}.permissions".format(app))
+                for name, obj in inspect.getmembers(lib):
+                    if type(obj) == type(Permission) and issubclass(obj, Permission)\
+                       and obj.__base__ != ABC:
+                        perm, created_at = obj.get_or_create()
+
+def create_groups(self, *args, **options):
+    create_permissions()
+
+    if not getattr(settings, 'DEFAULT_GROUPS_MODULE', None):
+        raise AttributeError("DEFAULT_GROUPS_MODULE setting is not set!")
+
+    else:
+        groups_module = settings.DEFAULT_GROUPS_MODULE
+
+    lib = importlib.import_module(groups_module)
+    for name, obj in inspect.getmembers(lib):
+        if type(obj) == type(Group) and issubclass(obj, Group)\
+           and obj.__base__ != ABC:
+            group, created_at = obj.get_or_create()
+            perm_db_objs = [
+                perm.get_or_create()[0] for perm in obj.permissions
+            ]
+            group.permissions.set(perm_db_objs)
+
+def setup_permissions():
+    # TODO: Add test
+    create_groups()
--- serious_django_permissions/management/commands/create_permissions.py
+++ serious_django_permissions/management/commands/create_permissions.py
@@ -1,11 +1,5 @@
-from abc import ABC
-import importlib
-import inspect
+from ... import helpers
 
-from django.core.management.base import BaseCommand, CommandError
-from django.conf import settings
-
-from serious_django_permissions.permissions import Permission
 
 class Command(BaseCommand):
     """
@@ -15,16 +9,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """
-            This method iterates through all installed apps and searches for 
-            classes that are inheriting our Permission class. 
+            This method iterates through all installed apps and searches for
+            classes that are inheriting our Permission class.
             If it finds one it creates the permission in the django model
         """
-        for app in settings.INSTALLED_APPS:
-            lib = importlib.util.find_spec("{}.permissions".format(app))
-            if lib:
-                lib = importlib.import_module("{}.permissions".format(app))
-                for name, obj in inspect.getmembers(lib):
-                    if type(obj) == type(Permission) and issubclass(obj, Permission)\
-                       and obj.__base__ != ABC:
-                        perm, created_at = obj.get_or_create()
-                
+        helpers.create_permissions(self, *args, **options)
